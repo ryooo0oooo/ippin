5c3086e13b15d25a7002caf86bee4f084e804786
村田諒 <ryomurata@murataryou-no-MacBook-Pro-2.local>
2016-06-25 16:57:59 +0900

    バリデーション追加で必須項目化

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,9 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 has_attached_file :icon,
-                      styles:  { medium: "300x300#", thumb: "100x100#" }
+      styles:  { medium: "300x300#", thumb: "100x100#" }
   validates_attachment_content_type :icon,
-                                      content_type: ["image/jpg","image/jpeg","image/png"]
+      content_type: ["image/jpg","image/jpeg","image/png"]
+
+ validates :account_id, presence: true
 end
