16c341ce218ddb332353d949734c568fa39a7ac5
村田諒 <ryomurata@murataryou-no-MacBook-Pro-2.local>
2016-06-26 12:54:41 +0900

    マイグレーションファイル編集&実行

--- db/migrate/20160626030058_create_posts.rb
+++ db/migrate/20160626030058_create_posts.rb
@@ -1,8 +1,11 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
-
-      t.timestamps null: false
+      t.text :image
+      t.text :description
+      t.integer :shop_id
+      t.integer :account_id
+      t.timestamps
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160625090438) do
+ActiveRecord::Schema.define(version: 20160626030058) do
 
   create_table "notes", force: :cascade do |t|
     t.string   "title",      limit: 255
     t.text     "text",       limit: 65535
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.text     "image",       limit: 65535
+    t.text     "description", limit: 65535
+    t.integer  "shop_id",     limit: 4
+    t.integer  "account_id",  limit: 4
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: :cascade do |t|
