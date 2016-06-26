1166f9dcdc189e8e05e839154f797c50672b45ad
村田諒 <ryomurata@murataryou-no-MacBook-Pro-2.local>
2016-06-26 16:19:23 +0900

    投稿内容(imageとdescription)をviewに反映(image タグを使用)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all    # 投稿内容を表示したい
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+  root 'posts#index'
+  resources :posts do
   resources :users, only: :show
-  resources :posts, only: :index
  end
+end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
