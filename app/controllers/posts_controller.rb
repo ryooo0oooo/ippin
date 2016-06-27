c7069e53a6fdb76e90be08037367b5e69bbce969
村田諒 <ryomurata@murataryou-no-MacBook-Pro-2.local>
2016-06-27 21:50:56 +0900

    マイページの追加や投稿の削除と編集機能などなど

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,30 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all    # 投稿内容を表示したい
+    @posts = Post.all.order("id DESC")   # 投稿内容を表示したい
   end
 
   def new
-    @post=Post.new
+    @post = Post.new
   end
+
+  def create
+    @post = Post.create(create_parmas)
+    redirect_to action: :index
+  end
+
+  def destroy
+    post =Post.find(params[:id])
+    if post.user_id == current_user.id
+      post.destroy
+    end
+  end
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+private
+def create_params
+  params.require(:post).permit(:image, :descripition).merge(user_id: current_user.id)
+end
+
 end
