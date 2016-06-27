940af7ea8c3688318fec235d1f617cff878c4d42
村田諒 <ryomurata@murataryou-no-MacBook-Pro-2.local>
2016-06-27 12:12:59 +0900

    投稿フォーム作る試し ここではネストいらない気がするので切ってみる

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
    root 'posts#index'
-  resources :posts do
+  resources :posts
   resources :users, only: :show
- end
 end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
