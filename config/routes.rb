d83c373ba4fd40e1158fc050f5a3d65135678c04
村田諒 <ryomurata@murataryou-no-MacBook-Pro-2.local>
2016-06-28 20:35:23 +0900

    コメントコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
    root 'posts#index'
-  resources :posts
+  resources :posts do
+    resources :comments, only: [:create]
+  end
   resources :users, only: :show
 end
   # The priority is based upon order of creation: first created -> highest priority.
